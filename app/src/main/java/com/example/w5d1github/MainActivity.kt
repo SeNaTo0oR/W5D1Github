846dec17008d595c9c6abc266f93ef565de31090
SeNaTo0oR <eng.fadi.daouk@gmail.com>
2021-11-28 15:45:44 +0300

    new

--- app/src/main/java/com/example/w5d1github/MainActivity.kt
+++ app/src/main/java/com/example/w5d1github/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.w5d1github
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    var b:Button?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
